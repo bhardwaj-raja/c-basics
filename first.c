68c58c61f415e90b1e2b5e1f69718167105e57dd
bhardwaj-raja <rajabhardwaj814@gmail.com>
2025-02-03 18:56:23 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float a=10,b;
+    // b=1/2*a;
+    // printf("%f",b);//0.000000
+    
+    // b=a*1/2;
+    // printf("%f",b);//5.000000
+    
+    // b=1.0/2*a;
+    // printf("%f",b);//5.000000
+    
+    // b=(float)1/2*a;
+    // printf("%f",b);//5.000000
+    return 0;
+}
