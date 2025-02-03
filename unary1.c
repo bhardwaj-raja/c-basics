d8eddcd1c1f86b8b29acf21918005f094cf36bd2
bhardwaj-raja <rajabhardwaj814@gmail.com>
2025-02-03 19:11:28 +0530

    Create unary1.c

---
+++ unary1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,x;
+    
+    // x=++a + ++a;
+    // printf("%d",x);//24
+    
+    // x=--a +--a;
+    // printf("%d",x);//16
+    
+    // x=--a +a--;
+    // printf("%d",x);//17
+    
+    // x=a-- + --a;
+    // printf("%d",x);//18
+    return 0;
+}
